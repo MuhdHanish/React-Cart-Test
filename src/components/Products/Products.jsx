2379053e9d0367ae375aef078636edf97a1b98c9
MuhdHanish <muhammedhanish11@gmail.com>
2023-10-16 21:34:35 +0530

    style(component): refactor inline styles to use external CSS in product componet

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,11 @@
 import Data from "../../Data/Data";
+import "./Products.css"
 import ProductCard from "./ProductCard/ProductCard";
 
 const Products = () => {
   return (
     <div className="pt-10">
-      <div className="grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-10">
+      <div className="products">
         {Data.slice(1,5)?.map((data, index) => (
           <ProductCard key={index} data={data} />
         ))}
