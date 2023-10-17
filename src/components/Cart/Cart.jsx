daa73c19a831111e954a1ca789220686ea324a5a
MuhdHanish <muhammedhanish11@gmail.com>
2023-10-17 08:50:53 +0530

    style(cart): add shadow to increase and decrease and product remove button

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = () => {
       </div>
       <div className="w-full h-fit flex flex-col justify-start items-start gap-4">
         <div className="w-full sm:w-2/3  flex justify-between items-center border p-2 rounded-md">
-          <div className="flex ">
+          <div className="flex gap-5">
             <div>
               <img src={Dumb[0].imageUrl} width={100} height={100} alt="" />
             </div>
@@ -18,15 +18,15 @@ const Cart = () => {
               <div className="flex flex-col gap-1 justify-center items-start ">
                 <span className="text-[14px] font-medium">{Dumb[0].name}</span>
                 <span className="text-[11px] text-gray-500">
-                  {Dumb[0].description.slice(0, 21) + "..."}
+                  {Dumb[0].description.slice(0, 30) + "..."}
                 </span>
               </div>
               <div className="flex w-full h-fit justify-start items-center text-[14px] gap-3">
-                <button className="p-1 rounded-full border w-6 h-6 flex justify-center items-center outline-none bg-gray-100">
+                <button className="p-1 shadow-md rounded-full border w-6 h-6 flex justify-center items-center outline-none bg-gray-100">
                   +
                 </button>
                 <span className="font-medium text-[13px]">4</span>
-                <button className="p-1 rounded-full border w-6 h-6 flex justify-center items-center outline-none bg-gray-100">
+                <button className="p-1 shadow-md rounded-full border w-6 h-6 flex justify-center items-center outline-none bg-gray-100">
                   -
                 </button>
               </div>
@@ -34,11 +34,11 @@ const Cart = () => {
           </div>
           <div className="flex gap-5 items-center px-5">
             <span className="font-medium">₹ {Dumb[0].discountAmount}</span>
-            <button className="p-1 rounded-full border w-6 h-6 flex justify-center items-center outline-none bg-gray-100 text-[12px]">
+            <button className="p-1 shadow-md   rounded-full border w-6 h-6 flex justify-center items-center outline-none bg-gray-100 text-[12px]">
               x
             </button>
           </div>
-              </div>
+        </div>
       </div>
     </div>
   );
